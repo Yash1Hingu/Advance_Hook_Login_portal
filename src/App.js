4219d34d0b5d7f91c346201bd2547c98bc5c1e43
Yash1Hingu <yash23hingu@gmail.com>
2023-05-25 10:00:05 +0530

    UPDATE -- break the props chain and create context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import LoginContext from './components/store/login-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+
   useEffect(() => {
     const userLoggedInInformation = localStorage.getItem('isLoggedIn');
-    if(userLoggedInInformation === '1') {
+    if (userLoggedInInformation === '1') {
       setIsLoggedIn(true);
     }
-  },[])
+  }, [])
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn','1');
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
@@ -27,13 +28,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <LoginContext.Provider
+      value={
+        { isLoggedIn: isLoggedIn }
+      }
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </LoginContext.Provider>
   );
 }
 
